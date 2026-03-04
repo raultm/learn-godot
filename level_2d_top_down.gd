03bef15423933a71ddcd20bdb8be4e754d51106f
Raul Tierno <raultm@gmail.com>
2026-03-04 19:39:22 +0100

    feat: :sparkles: añadir movimiento al jugador

--- level_2d_top_down.gd
+++ level_2d_top_down.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var tile_size : int = 32
 
 var grid := []
+var player := ColorRect.new()  # variable global para guardar el jugador
 
 func _ready():
 	if level_path == "":
@@ -58,8 +59,28 @@ func spawn_floor(pos):
 	add_child(rect)
 
 func spawn_player(pos):
-	var rect = ColorRect.new()
-	rect.color = Color.GREEN
-	rect.size = Vector2(tile_size, tile_size)
-	rect.position = pos
-	add_child(rect)
+	player.color = Color.GREEN
+	player.size = Vector2(tile_size, tile_size)
+	player.position = pos
+	player.z_index = 10 
+	add_child(player)
+	
+func _process(delta):
+	if player == null:
+		return  # todavía no se ha creado el jugador
+
+	var move = Vector2.ZERO
+
+	# Movimiento simple con flechas
+	if Input.is_action_pressed("ui_up"):
+		move.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		move.y += 1
+	if Input.is_action_pressed("ui_left"):
+		move.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		move.x += 1
+
+	if move != Vector2.ZERO:
+		move = move.normalized() * tile_size  # mover por tamaño de tile
+		player.position += move
